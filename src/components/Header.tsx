3177c2cd14807a44a478ce80a2882ae9c825e5fe
brcls <petrick_barcelos@hotmail.com>
2023-10-23 16:58:37 -0300

    melhorando build e consertando alguns pontos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,8 @@ export default function Header() {
       <Link href={"/"}>
         <button
           className="text-zinc-100 py-2 px-6 
-        rounded-full font-bold hover:scale-110 duration-500 hover:bg-zinc-900 active:scale-95"
+        rounded-full font-bold hover:scale-110 duration-500 
+        hover:bg-zinc-900 active:scale-95"
         >
           Projects
         </button>
@@ -20,7 +21,8 @@ export default function Header() {
       <Link href={"/about-me"}>
         <button
           className="text-zinc-100 py-2 px-6 
-        rounded-full font-bold hover:scale-110 duration-500 hover:bg-zinc-900 active:scale-95"
+        rounded-full font-bold hover:scale-110 duration-500 
+        hover:bg-zinc-900 active:scale-95"
         >
           About me
         </button>
@@ -28,7 +30,8 @@ export default function Header() {
       <Link href={"/contact"}>
         <button
           className="text-zinc-100 py-2 px-6 
-        rounded-full font-bold hover:scale-110 duration-500 hover:bg-zinc-900 active:scale-95"
+        rounded-full font-bold hover:scale-110 duration-500 
+        hover:bg-zinc-900 active:scale-95"
         >
           Contact
         </button>
