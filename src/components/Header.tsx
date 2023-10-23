0f4ad6402918d92e0fb058ccfaf87605abad64b4
brcls <petrick_barcelos@hotmail.com>
2023-10-23 16:40:26 -0300

    feat: alinhado header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header
       className="bg-zinc-700 backdrop-filter backdrop-blur-sm bg-opacity-25 
-                    flex mt-4 justify-center align-middle gap-2 
+                    flex mt-4 justify-center items-center gap-2 
                     p-1 md:w-1/2 inset-x-0 mx-auto rounded-full fixed z-10
                     w-11/12"
     >
