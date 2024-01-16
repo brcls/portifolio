be529514b82785d25444edf4f0203cc91b88e013
brcls <petrick_barcelos@hotmail.com>
2024-01-16 12:46:50 -0300

    melhorado o alert

--- src/components/atoms/Alert.tsx
+++ src/components/atoms/Alert.tsx
@@ -9,34 +9,18 @@ export default function Alert() {
     <>
       {showAlert && (
         <div
-          role="alert"
-          className="alert z-50 glass-dark bg-[#6365f184] border-none fixed bottom-4 right-1/2 
-      w-11/12 translate-x-1/2"
+          className="p-4 z-50 glass-dark bg-[#6365f184] border-none fixed bottom-4 right-1/2 
+      w-11/12 translate-x-1/2 flex justify-between text-start items-center rounded-xl gap-4"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="stroke-info shrink-0 w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            ></path>
-          </svg>
-          <span>
+          <p>
             This portfolio is not yet finalized, but one day it will be hehe
-          </span>
-          <div>
-            <button
-              className="btn btn-sm btn-primary"
-              onClick={() => setShowAlert(false)}
-            >
-              Close
-            </button>
-          </div>
+          </p>
+          <button
+            className="btn btn-sm btn-primary"
+            onClick={() => setShowAlert(false)}
+          >
+            Close
+          </button>
         </div>
       )}
     </>
