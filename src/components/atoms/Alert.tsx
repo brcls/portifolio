05249a90060884a91aae091af5c5227b384a62b5
brcls <petrick_barcelos@hotmail.com>
2024-01-17 17:19:14 -0300

    melhorado o alert

--- src/components/atoms/Alert.tsx
+++ src/components/atoms/Alert.tsx
@@ -5,14 +5,20 @@ import Text from "./Text";
 
 export default function Alert() {
   const [showAlert, setShowAlert] = useState(true);
+  const [animate, setAnimate] = useState(false);
+
+  const handleClose = () => {
+    setAnimate(true);
+    setTimeout(() => setShowAlert(false), 600);
+  };
 
   return (
     <>
       {showAlert && (
         <div
-          className="p-4 z-50 glass-dark border-none fixed bottom-4 right-1/2 
-      w-11/12 translate-x-1/2 flex md:flex-row flex-col justify-between text-start items-center rounded-xl gap-4
-      transition duration-1000"
+          className={`p-4 z-50 glass-dark border-none fixed bottom-4 right-1/2 
+          w-11/12 translate-x-1/2 flex md:flex-row flex-col justify-between text-start items-center rounded-xl gap-4
+          transition duration-700 transform ${animate && "opacity-0 scale-90"}`}
         >
           <Text>
             This portfolio is a work in progress, but rest assured, one day it
@@ -20,8 +26,8 @@ export default function Alert() {
           </Text>
           <button
             className="py-2 px-4 rounded-full hover:scale-105 active:scale-95 transition duration-500 
-        hover:bg-zinc-800 active:bg-zinc-900 border-zinc-900/10 border-2 md:w-min w-full"
-            onClick={() => setShowAlert(false)}
+            hover:bg-zinc-800 active:bg-zinc-900 border-zinc-100/20 border-2 md:w-min w-full"
+            onClick={handleClose}
           >
             <Text>Close</Text>
           </button>
