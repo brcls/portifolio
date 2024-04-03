51e4f18d22aac30e69bf8140f0498ba37d257265
brcls <petrick_barcelos@hotmail.com>
2024-04-03 09:24:25 -0300

    hiding projects

--- public/data/projects/projects.ts
+++ public/data/projects/projects.ts
@@ -29,43 +29,43 @@ export const projects: IProject[] = [
     projectVisualization: tripperVisualization,
     coverImage: TripperCapa,
   },
-  {
-    slug: "dost",
-    description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
-                  landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
-                  the added feature of voting on routes to elevate their visibility within the community.`,
-    name: "DOST",
-    techStack: ["React", "TypeScript", "Tailwind", "Next.js"],
-    timeline: null,
-    link: "https://dostproject.vercel.app/",
-    members: ["Erick Barcelos"],
-    projectVisualization: tripperVisualization,
-    coverImage: TripperCapa,
-  },
-  {
-    slug: "joystick",
-    description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
-                  landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
-                  the added feature of voting on routes to elevate their visibility within the community.`,
-    name: "Joystick",
-    techStack: ["React", "JavaScript", "MongoDB", "Node.js"],
-    timeline: null,
-    gitLink: "https://github.com/brcls/joystick",
-    members: ["Erick Barcelos"],
-    projectVisualization: tripperVisualization,
-    coverImage: TripperCapa,
-  },
-  {
-    slug: "secret-santa",
-    description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
-                  landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
-                  the added feature of voting on routes to elevate their visibility within the community.`,
-    name: "Secret Santa",
-    techStack: ["React", "JavaScript", "MongoDB", "Node.js"],
-    timeline: null,
-    gitLink: "https://github.com/brcls/amigo-secreto",
-    members: ["Erick Barcelos"],
-    projectVisualization: tripperVisualization,
-    coverImage: TripperCapa,
-  },
+  // {
+  // slug: "dost",
+  //   description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
+  //                 landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
+  //                 the added feature of voting on routes to elevate their visibility within the community.`,
+  //   name: "DOST",
+  //   techStack: ["React", "TypeScript", "Tailwind", "Next.js"],
+  //   timeline: null,
+  //   link: "https://dostproject.vercel.app/",
+  //   members: ["Erick Barcelos"],
+  //   projectVisualization: tripperVisualization,
+  //   coverImage: TripperCapa,
+  // },
+  // {
+  //   slug: "joystick",
+  //   description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
+  //                 landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
+  //                 the added feature of voting on routes to elevate their visibility within the community.`,
+  //   name: "Joystick",
+  //   techStack: ["React", "JavaScript", "MongoDB", "Node.js"],
+  //   timeline: null,
+  //   gitLink: "https://github.com/brcls/joystick",
+  //   members: ["Erick Barcelos"],
+  //   projectVisualization: tripperVisualization,
+  //   coverImage: TripperCapa,
+  // },
+  // {
+  //   slug: "secret-santa",
+  //   description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
+  //                 landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
+  //                 the added feature of voting on routes to elevate their visibility within the community.`,
+  //   name: "Secret Santa",
+  //   techStack: ["React", "JavaScript", "MongoDB", "Node.js"],
+  //   timeline: null,
+  //   gitLink: "https://github.com/brcls/amigo-secreto",
+  //   members: ["Erick Barcelos"],
+  //   projectVisualization: tripperVisualization,
+  //   coverImage: TripperCapa,
+  // },
 ];
